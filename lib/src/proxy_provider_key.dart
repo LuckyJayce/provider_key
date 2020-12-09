c02f4ea8169a2b0bd67abfb3f89896969b1758aa
Jayce <>
2020-12-09 17:30:52 +0800

    ChangeNotifierProxyProviderKey3 ChangeNotifierProxyProviderKey4 ChangeNotifierProxyProviderKey5

--- lib/src/proxy_provider_key.dart
+++ lib/src/proxy_provider_key.dart
@@ -55,9 +55,6 @@ class ChangeNotifierProxyProviderKey<T, R extends ChangeNotifier>
 
 class ChangeNotifierProxyProviderKey2<T, T2, R extends ChangeNotifier>
     extends ProviderKey<R> {
-  final T name = null;
-  final T2 name2 = null;
-  final R name3 = null;
   const ChangeNotifierProxyProviderKey2.build(
       {@required ProviderKey<T> dependKey1,
       @required ProviderKey<T2> dependKey2,
@@ -72,6 +69,90 @@ class ChangeNotifierProxyProviderKey2<T, T2, R extends ChangeNotifier>
     Widget child,
   }) {
     return ChangeNotifierProxyProvider2<T, T2, R>(
+      key: key,
+      create: create,
+      update: update,
+      lazy: lazy,
+      builder: builder,
+      child: child,
+    );
+  }
+}
+
+class ChangeNotifierProxyProviderKey3<T, T2, T3, R extends ChangeNotifier>
+    extends ProviderKey<R> {
+  const ChangeNotifierProxyProviderKey3.build(
+      {@required ProviderKey<T> dependKey1,
+      @required ProviderKey<T2> dependKey2,
+      @required ProviderKey<T3> dependKey3,
+      @required ProviderType<R> selfType});
+
+  ChangeNotifierProxyProvider3<T, T2, T3, R> provider(
+    Create<R> create,
+    ProxyProviderBuilder3<T, T2, T3, R> update, {
+    Key key,
+    bool lazy,
+    TransitionBuilder builder,
+    Widget child,
+  }) {
+    return ChangeNotifierProxyProvider3<T, T2, T3, R>(
+      key: key,
+      create: create,
+      update: update,
+      lazy: lazy,
+      builder: builder,
+      child: child,
+    );
+  }
+}
+
+class ChangeNotifierProxyProviderKey4<T, T2, T3, T4, R extends ChangeNotifier>
+    extends ProviderKey<R> {
+  const ChangeNotifierProxyProviderKey4.build(
+      {@required ProviderKey<T> dependKey1,
+      @required ProviderKey<T2> dependKey2,
+      @required ProviderKey<T3> dependKey3,
+      @required ProviderKey<T4> dependKey4,
+      @required ProviderType<R> selfType});
+
+  ChangeNotifierProxyProvider4<T, T2, T3, T4, R> provider(
+    Create<R> create,
+    ProxyProviderBuilder4<T, T2, T3, T4, R> update, {
+    Key key,
+    bool lazy,
+    TransitionBuilder builder,
+    Widget child,
+  }) {
+    return ChangeNotifierProxyProvider4<T, T2, T3, T4, R>(
+      key: key,
+      create: create,
+      update: update,
+      lazy: lazy,
+      builder: builder,
+      child: child,
+    );
+  }
+}
+
+class ChangeNotifierProxyProviderKey5<T, T2, T3, T4, T5,
+    R extends ChangeNotifier> extends ProviderKey<R> {
+  const ChangeNotifierProxyProviderKey5.build(
+      {@required ProviderKey<T> dependKey1,
+      @required ProviderKey<T2> dependKey2,
+      @required ProviderKey<T3> dependKey3,
+      @required ProviderKey<T4> dependKey4,
+      @required ProviderKey<T4> dependKey5,
+      @required ProviderType<R> selfType});
+
+  ChangeNotifierProxyProvider5<T, T2, T3, T4, T5, R> provider(
+    Create<R> create,
+    ProxyProviderBuilder5<T, T2, T3, T4, T5, R> update, {
+    Key key,
+    bool lazy,
+    TransitionBuilder builder,
+    Widget child,
+  }) {
+    return ChangeNotifierProxyProvider5<T, T2, T3, T4, T5, R>(
       key: key,
       create: create,
       update: update,
