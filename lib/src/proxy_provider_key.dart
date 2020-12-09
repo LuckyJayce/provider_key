ef6227da955c574ea1f87c2a746366fb5734d24d
Jayce <>
2020-12-09 17:39:12 +0800

    ProxyProviderKey ProxyProviderKey2 ProxyProviderKey3 ProxyProviderKey4 ProxyProviderKey5

--- lib/src/proxy_provider_key.dart
+++ lib/src/proxy_provider_key.dart
@@ -29,6 +29,158 @@ class ListenableProxyProviderKey<T, R extends Listenable>
   }
 }
 
+class ProxyProviderKey<T, R extends Listenable> extends ProviderKey<R> {
+  const ProxyProviderKey.build(
+      {@required ProviderKey<T> dependKey, @required ProviderType<R> selfType});
+
+  ProxyProvider<T, R> provider(
+    Create<R> create, {
+    Key key,
+    @required ProxyProviderBuilder<T, R> update,
+    UpdateShouldNotify<R> updateShouldNotify,
+    Dispose<R> dispose,
+    bool lazy,
+    TransitionBuilder builder,
+    Widget child,
+  }) {
+    return ProxyProvider<T, R>(
+      key: key,
+      create: create,
+      update: update,
+      updateShouldNotify: updateShouldNotify,
+      dispose: dispose,
+      lazy: lazy,
+      builder: builder,
+      child: child,
+    );
+  }
+}
+
+class ProxyProviderKey2<T, T2, R extends Listenable> extends ProviderKey<R> {
+  const ProxyProviderKey2.build(
+      {@required ProviderKey<T> dependKey1,
+      @required ProviderKey<T2> dependKey2,
+      @required ProviderType<R> selfType});
+
+  ProxyProvider2<T, T2, R> provider(
+    Create<R> create, {
+    Key key,
+    @required ProxyProviderBuilder2<T, T2, R> update,
+    UpdateShouldNotify<R> updateShouldNotify,
+    Dispose<R> dispose,
+    bool lazy,
+    TransitionBuilder builder,
+    Widget child,
+  }) {
+    return ProxyProvider2<T, T2, R>(
+      key: key,
+      create: create,
+      update: update,
+      updateShouldNotify: updateShouldNotify,
+      dispose: dispose,
+      lazy: lazy,
+      builder: builder,
+      child: child,
+    );
+  }
+}
+
+class ProxyProviderKey3<T, T2, T3, R extends Listenable>
+    extends ProviderKey<R> {
+  const ProxyProviderKey3.build(
+      {@required ProviderKey<T> dependKey1,
+      @required ProviderKey<T2> dependKey2,
+      @required ProviderKey<T3> dependKey3,
+      @required ProviderType<R> selfType});
+
+  ProxyProvider3<T, T2, T3, R> provider(
+    Create<R> create, {
+    Key key,
+    @required ProxyProviderBuilder3<T, T2, T3, R> update,
+    UpdateShouldNotify<R> updateShouldNotify,
+    Dispose<R> dispose,
+    bool lazy,
+    TransitionBuilder builder,
+    Widget child,
+  }) {
+    return ProxyProvider3<T, T2, T3, R>(
+      key: key,
+      create: create,
+      update: update,
+      updateShouldNotify: updateShouldNotify,
+      dispose: dispose,
+      lazy: lazy,
+      builder: builder,
+      child: child,
+    );
+  }
+}
+
+class ProxyProviderKey4<T, T2, T3, T4, R extends Listenable>
+    extends ProviderKey<R> {
+  const ProxyProviderKey4.build(
+      {@required ProviderKey<T> dependKey1,
+      @required ProviderKey<T2> dependKey2,
+      @required ProviderKey<T3> dependKey3,
+      @required ProviderKey<T4> dependKey4,
+      @required ProviderType<R> selfType});
+
+  ProxyProvider4<T, T2, T3, T4, R> provider(
+    Create<R> create, {
+    Key key,
+    @required ProxyProviderBuilder4<T, T2, T3, T4, R> update,
+    UpdateShouldNotify<R> updateShouldNotify,
+    Dispose<R> dispose,
+    bool lazy,
+    TransitionBuilder builder,
+    Widget child,
+  }) {
+    return ProxyProvider4<T, T2, T3, T4, R>(
+      key: key,
+      create: create,
+      update: update,
+      updateShouldNotify: updateShouldNotify,
+      dispose: dispose,
+      lazy: lazy,
+      builder: builder,
+      child: child,
+    );
+  }
+}
+
+class ProxyProviderKey5<T, T2, T3, T4, T5, R extends Listenable>
+    extends ProviderKey<R> {
+  const ProxyProviderKey5.build(
+      {@required ProviderKey<T> dependKey1,
+      @required ProviderKey<T2> dependKey2,
+      @required ProviderKey<T3> dependKey3,
+      @required ProviderKey<T4> dependKey4,
+      @required ProviderKey<T5> dependKey5,
+      @required ProviderType<R> selfType});
+
+  ProxyProvider5<T, T2, T3, T4, T5, R> provider(
+    Create<R> create, {
+    Key key,
+    @required ProxyProviderBuilder5<T, T2, T3, T4, T5, R> update,
+    UpdateShouldNotify<R> updateShouldNotify,
+    Dispose<R> dispose,
+    bool lazy,
+    TransitionBuilder builder,
+    Widget child,
+  }) {
+    return ProxyProvider5<T, T2, T3, T4, T5, R>(
+      key: key,
+      create: create,
+      update: update,
+      updateShouldNotify: updateShouldNotify,
+      dispose: dispose,
+      lazy: lazy,
+      builder: builder,
+      child: child,
+    );
+  }
+}
+
 class ChangeNotifierProxyProviderKey<T, R extends ChangeNotifier>
     extends ProviderKey<R> {
   const ChangeNotifierProxyProviderKey.build(
